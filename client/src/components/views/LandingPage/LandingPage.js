f9952489a725bb6ce4d8936c891610b45c8252b4
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-06-17 14:28:58 -0400

    Video commit

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { FaCode } from "react-icons/fa";
 
+
+
 function LandingPage() {
     return (
         <>
         <div className="app">
             <FaCode style={{ fontSize: '4rem' }} /><br />
+            {/* <section style={ sectionStyle }>
+            </section> */}
             <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+            </div>
         </>
     )
 }
+
 
 export default LandingPage
